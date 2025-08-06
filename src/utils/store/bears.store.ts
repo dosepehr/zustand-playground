dfd34c6d4679137e81a848441cea8def653f0abf
dosepehr <dosepehr@gmail.com>
2025-08-06 14:55:52 +0330

    add devtools

--- src/utils/store/bears.store.ts
+++ src/utils/store/bears.store.ts
@@ -1,5 +1,5 @@
 import { create } from 'zustand';
-
+import { devtools } from 'zustand/middleware';
 type BearsStore = {
     bears: number;
     increasePopulation: () => void;
@@ -7,9 +7,11 @@ type BearsStore = {
     updateBears: (newBears: number) => void;
 };
 
-export const useBearsStore = create<BearsStore>((set, get) => ({
-    bears: 0,
-    increasePopulation: () => set((state) => ({ bears: state.bears + 1 })),
-    removeAllBears: () => set({ bears: 0 }),
-    updateBears: (newBears) => set({ bears: newBears + get().bears }),
-}));
+export const useBearsStore = create<BearsStore>()(
+    devtools((set, get) => ({
+        bears: 0,
+        increasePopulation: () => set((state) => ({ bears: state.bears + 1 })),
+        removeAllBears: () => set({ bears: 0 }),
+        updateBears: (newBears) => set({ bears: newBears + get().bears }),
+    })),
+);
